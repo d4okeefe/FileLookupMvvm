a848bb6e63c7a7314e6422a9787cc92660774021
d4okeefe@gmail.com <d4okeefe@gmail.com>
2023-01-18 18:08:34 -0600

    1/18/23 2nd Evening Commit

---
+++ FileSearchMvvm/Models/CockleTypes/MSWordFile.cs
@@ -0,0 +1,15 @@
+﻿namespace FileSearchMvvm.Models.CockleTypes
+{
+    public class MSWordFile
+    {
+        public string Filename { get; set; }
+
+        public MSWordFile(string filename)
+        {
+
+            Filename = filename;
+
+
+        }
+    }
+}
